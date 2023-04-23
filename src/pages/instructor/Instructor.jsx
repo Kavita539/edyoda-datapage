3a2957f9e06574aace0d8a716075d7e1e5bf3ef6
Kavita Pathak <pathakkavita273@gmail.com>
2023-04-23 21:09:16 +0530

    feat: Instructor Page

--- src/pages/instructor/Instructor.jsx
+++ src/pages/instructor/Instructor.jsx
@@ -1,7 +1,20 @@
+import CourseAccordion from "../../Components/courseAccordion/CourseAccordion";
+import NavBar from "../../Components/navbar/NavBar";
+import SideBar from "../../Components/sidebar/SideBar";
+import "./instructor.css";
 
 const Instructor = () => {
   return (
-    <div>Instructor</div>
+    <>
+      <NavBar />
+      <CourseAccordion />
+      <div className="instructor-area">
+        <SideBar />
+        <div className="instructor-content">
+          <h2>Nothing to display here...</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
